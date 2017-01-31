cd56b24aebb5c8eb2ba88cce42d7a34db579f437
Julian Gruber <julian@juliangruber.com>
2017-01-31 16:50:01 +0100

    add --electron-{filter,versions} to build for electron

--- index.js
+++ index.js
@@ -3,6 +3,8 @@
 var exec = require('child_process').exec
 var spawn = require('cross-spawn')
 var npmRunPath = require('npm-run-path-compat')
+var minimist = require('minimist')
+var os = require('os')
 
 if (!process.env.CI) process.exit()
 
@@ -12,7 +14,14 @@ if (!token) {
   process.exit(0)
 }
 
-function version (rev, cb) {
+var argv = minimist(process.argv.slice(2), {
+  alias: {
+    electronFilter: 'electron-filter',
+    electronVersions: 'electron-versions'
+  }
+})
+
+function getPackageVersion (rev, cb) {
   exec('git show ' + rev + ':package.json', {
     encoding: 'utf8'
   }, function (err, diff) {
@@ -20,10 +29,11 @@ function version (rev, cb) {
   })
 }
 
-function prebuild (cb) {
+function prebuild (runtime, version, cb) {
   var ps = spawn('prebuild', [
     '-b', process.version,
     '-u', token,
+    '-r', runtime,
     '--verbose'
   ], {
     env: npmRunPath.env()
@@ -36,14 +46,41 @@ function prebuild (cb) {
   })
 }
 
-version('HEAD', function (err, head) {
+getPackageVersion('HEAD', function (err, head) {
   if (err) throw err
-  version('HEAD~1', function (err, prev) {
+
+  getPackageVersion('HEAD~1', function (err, prev) {
     if (err) throw err
     if (head === prev) process.exit(0)
 
-    prebuild(function (err, code) {
-      if (err) return process.exit(code)
+    prebuild('node', process.version, function (err, code) {
+      if (err) process.exit(code)
+      if (!argv.electronFilter || !argv.electronVersions) process.exit(0)
+      if (typeof argv.electronFilter === 'string') {
+        var args = argv.electronFilter.split(':')
+
+        var version = args.shift()
+        if (version && process.version.slice(1, 1 + version.length) !== version) {
+          process.exit(0)
+        }
+
+        var platform = args.shift()
+        if (platform && os.platform() !== platform) process.exit(0)
+
+        var arch = args.shift()
+        if (arch && os.arch() !== arch) process.exit(0)
+      }
+
+      var versions = argv.electronVersions.split(',')
+      function next () {
+        var version = versions.shift()
+        if (!version) return process.exit(0)
+        prebuild('electron', version, function (err, code) {
+          if (err) process.exit(code)
+          next()
+        })
+      }
+      next()
     })
   })
 })
