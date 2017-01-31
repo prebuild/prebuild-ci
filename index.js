e9b8e216df9c9788dcda58f84f26c2c1fe80f360
Julian Gruber <julian@juliangruber.com>
2017-01-31 16:31:12 +0100

    refactor prebuild command into function

--- index.js
+++ index.js
@@ -20,23 +20,30 @@ function version (rev, cb) {
   })
 }
 
+function prebuild (cb) {
+  var ps = spawn('prebuild', [
+    '-b', process.version,
+    '-u', token,
+    '--verbose'
+  ], {
+    env: npmRunPath.env()
+  })
+  ps.stdout.pipe(process.stdout)
+  ps.stderr.pipe(process.stderr)
+  ps.on('exit', function (code) {
+    if (code) return cb(Error(), code)
+    cb()
+  })
+}
+
 version('HEAD', function (err, head) {
   if (err) throw err
   version('HEAD~1', function (err, prev) {
     if (err) throw err
     if (head === prev) process.exit(0)
 
-    var ps = spawn('prebuild', [
-      '-b', process.version,
-      '-u', token,
-      '--verbose'
-    ], {
-      env: npmRunPath.env()
-    })
-    ps.stdout.pipe(process.stdout)
-    ps.stderr.pipe(process.stderr)
-    ps.on('exit', function (code) {
-      process.exit(code)
+    prebuild(function (err, code) {
+      if (err) return process.exit(code)
     })
   })
 })
