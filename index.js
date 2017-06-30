4126c6de687933880ef461558a6eadfc3687016c
Julian Gruber <julian@juliangruber.com>
2017-06-30 08:58:53 +0200

    add oddball electron version 53

--- index.js
+++ index.js
@@ -59,19 +59,21 @@ getPackageVersion('HEAD', function (err, head) {
     prebuild('node', process.versions.modules, function (err, code) {
       if (err) process.exit(code)
 
-      log.info('build', 'Trying oddball electron version')
+      log.info('build', 'Trying oddball electron versions')
       prebuild('electron', '50', function () {
-        try {
-          getTarget(process.versions.modules, 'electron')
-        } catch (err) {
-          log.info('No matching electron version, exiting')
-          process.exit(0)
-        }
+        prebuild('electron', '53', function () {
+          try {
+            getTarget(process.versions.modules, 'electron')
+          } catch (err) {
+            log.info('No matching electron version, exiting')
+            process.exit(0)
+          }
 
-        prebuild('electron', process.versions.modules, function (err, code) {
-          if (err) process.exit(code)
-          log.info('All done!')
-          process.exit(code)
+          prebuild('electron', process.versions.modules, function (err, code) {
+            if (err) process.exit(code)
+            log.info('All done!')
+            process.exit(code)
+          })
         })
       })
     })
